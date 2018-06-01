b15eeaa35677a96393f12ee54149daee6404eb91
Tomáš Krmela <openchange@gmail.com>
2018-06-01 13:34:34 +0200

    meta

--- meta/config.js
+++ meta/config.js
@@ -2,21 +2,21 @@
  * Created by vaibhav on 31/3/18
  */
 module.exports = {
-  siteTitle: 'Gatsby Starter Business', // Site title.
-  siteTitleAlt: 'Business', // Alternative site title for SEO.
+  siteTitle: 'Demo web by  Netmole', // Site title.
+  siteTitleAlt: 'krasa reactu v akci ', // Alternative site title for SEO.
   siteLogo: '/logos/logo-512x512.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-starter-business.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://demobynetmole.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-starter-business/.
-  siteDescription: 'Leverage Gatsby Business Starter for your Business.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'perfektni rychlost Javascriptu.', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml',
   siteFBAppID: '', // FB Application ID for using app insights
   googleTagManagerID: '', // GTM tracking ID.
-  disqusShortname: 'gatsby-business-starter', // Disqus shortname.
-  userName: 'Vaibhav Sharma',
-  userTwitter: 'vaibhaved',
-  userLocation: 'Delhi NCR, India',
-  userDescription: '',
-  copyright: 'Copyright © Gatsby Starter Business 2018. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
+  disqusShortname: 'nic', // Disqus shortname.
+  userName: 'NetmoleCZ',
+  userTwitter: 'NetmoleCZ',
+  userLocation: 'czechia',
+  userDescription: 'Skilled Web Dev',
+  copyright: 'Copyright © NetmoleCZ. All Rights Reserved.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#00d1b2', // Used for setting manifest and progress theme colors.
   backgroundColor: '#ffffff', // Used for setting manifest background color.
 }
